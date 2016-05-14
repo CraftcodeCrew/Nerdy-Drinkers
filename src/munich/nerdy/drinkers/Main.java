3957a76c3d011dfeb637fd95b5b4deb8ad586efb
Dominik Juelg <djuelg@gmx.de>
2016-05-14 23:03:04 +0200

    added some functionality

--- src/munich/nerdy/drinkers/Main.java
+++ src/munich/nerdy/drinkers/Main.java
@@ -3,11 +3,14 @@ package munich.nerdy.drinkers;
 public class Main {
 
     public static void main(String[] args) {
-	    int players= 0; // Insert from IO
-        String names[]= new String[players];
+	    int players= 0; //GameInput.insertNumberOfPlayers();
+        String names[]= {}; //= GameInput.insertPlayerNames();
 
         Game game= new Game(players, names);
 
+        while(game.getPlayOn()) {
+
+        }
 
     }
 }
--- src/munich/nerdy/drinkers/Round.java
+++ src/munich/nerdy/drinkers/Round.java
@@ -4,4 +4,8 @@ package munich.nerdy.drinkers;
  * Created by Domi on 14.05.2016.
  */
 public class Round {
+
+    public static int getRandomPlayer(int players) {
+        return (int)(Math.random() * ((players) + 1));
+    }
 }
